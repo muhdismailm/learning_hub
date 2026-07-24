f12ca7aa053a09a49b2e4a1b8746ad47afa2824a
muhdismailm <muhammedismailm772@gmail.com>
2026-07-24 10:59:43 +0530

    day14

---
+++ day14.py
@@ -0,0 +1,57 @@
+# #set
+
+# # s1={1,2,3,4,8,4,4,2,1,5}
+# # print(s1)
+# # s1.add(10)
+# # print(s1)
+# # s1.remove(2)
+# # print(s1)
+# # s1.discard(10)
+# # print(s1)
+
+# #dictionary
+
+# dict={
+#     "Name":"ismail",
+#     "dob":"12/4/2002",
+#     "date":8
+    
+# }
+
+# print(dict)
+
+
+# s="hello world"
+# vo="aeiou"
+# vowel=0
+# conso=0
+# for i in s:
+#     if i in vo:
+#         vowel+=1
+#     else:
+#         conso+=1
+        
+# dict={
+#     "vowels": vowel,
+#     "consonants": conso
+# }
+# print(dict)
+
+# dict1 = {"a": 10, "b": 20, "c": 30, "d": 40}
+# dict2 = {"b": 50, "c": 60, "e": 70}
+# set1=set()
+
+# for i in dict1:
+#     if i in dict2:
+#         set1.add(i)
+
+# print(set1)
+        
+        
+s= "my program language"
+dict1={}
+for i in s:
+    dict1.keys()
+    
+    
+print(dict1)
